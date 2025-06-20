eb01a5bc14bfcae8f625b2d28dcdfa88e55b9a2d
Nafissatou Ndir <nafissatoundir@MacBook-Pro-de-Nafissatou.local>
2025-06-20 15:16:44 +0400

    Tes changements

--- src/TowerNode.js
+++ src/TowerNode.js
@@ -16,8 +16,8 @@ export default function TowerNode({ data }) {
     }}>
       {/* Label positionné en absolute au-dessus si tu veux */}
       <div style={{
-  width: 700,
-  height: 700,
+  width: 550,
+  height: 550,
   flexShrink: 0,     // ✅ Clé : ne pas rétrécir !
   display: 'flex',
   justifyContent: 'center',
